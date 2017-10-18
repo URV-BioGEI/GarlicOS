a52364480ea932f09d98b7bb82570c47464ced52
Oscar Albert <oscar.albert@estudiants.urv.cat>
2017-10-19 01:26:36 +0200

    progG: afegides _gg_iniGrafA() i _gg_generarMarco()

--- GARLIC_OS/source/garlic_graf.c
+++ GARLIC_OS/source/garlic_graf.c
@@ -10,19 +10,94 @@
 #include <garlic_system.h>	// definición de funciones y variables de sistema
 #include <garlic_font.h>	// definición gráfica de caracteres
 
+/*definiciones*/
+#define NVENT	 4					// número de ventanas totales
+#define PPART	 2					// número de ventanas horizontales o verticales (particiones de pantalla)
+
+#define L2_PPART 1				// log base 2 de PPART
+
+#define VCOLS	 32				// columnas y filas de cualquier ventana
+#define VFILS	 24
+#define PCOLS	 VCOLS * PPART		// número de columnas totales (en pantalla)
+#define PFILS	 VFILS * PPART		// número de filas totales (en pantalla)
+
+int bg2, bg3;
 
 
 /* _gg_generarMarco: dibuja el marco de la ventana que se indica por parámetro*/
 void _gg_generarMarco(int v)
 {
-
+	//mapPtr=mapbase_bg3+desplazamiento de filas
+	u16 * mapPtr=bgGetMapPtr(bg3)+(((v)/PPART)*VFILS*2*VCOLS);
+	//si es impar mapPtr=mapPtr+numColumnas de 1 ventana
+	if(v%PPART!=0){
+		mapPtr=(u16*)mapPtr+VCOLS*(v%PPART);
+	}
+	//recorremos filas (i=y)
+	for (int i=0;i<VFILS;i++){
+		//recorremos columnas (j=x)
+		for(int j=0;j<VCOLS;j++){
+			if(i==0){
+				if(j==0)
+					mapPtr[j+i*64]=103;
+				else 
+				{
+				if(j==VCOLS-1)
+					mapPtr[j+i*64]=102;
+				else{
+					mapPtr[j+i*64]=99;
+					}
+				}
+			}
+			else if(i==VFILS-1){
+				if(j==0)
+					mapPtr[j+i*64]=100;
+				else 
+				{
+				if(j==VCOLS-1)
+					mapPtr[j+i*64]=101;
+				else
+					mapPtr[j+i*64]=97;
+					}
+				}
+			else{
+				if(j==0)
+					mapPtr[j+i*64]=96;
+				else if(j==VCOLS-1)
+					mapPtr[j+i*64]=98;
+				}
+		}
+	}
 }
 
 
 /* _gg_iniGraf: inicializa el procesador gráfico A para GARLIC 1.0 */
 void _gg_iniGrafA()
 {
-
+	videoSetMode(MODE_5_2D);
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+	lcdMainOnTop();
+	//inicialització del fons gràfics 2 i 3: S'ha de tenir en compte el tamany que ocupará cada mapa
+	//tamany mapa=nº posicions*nºbytes/posició=64*64posicions * 2bytes/posició =8192bytes= 8K (separació de 4 mapbase)
+	bg2=bgInit(2, BgType_ExRotation,BgSize_ER_512x512,0,3);
+	bg3=bgInit(3, BgType_ExRotation,BgSize_ER_512x512,4,3);
+	//Prioritat fons 3>fons 2
+	bgSetPriority(bg3,0);
+	bgSetPriority(bg2,1);
+	//void decompress(const void * data, void * dst, DecompressType type)
+	decompress(garlic_fontTiles, bgGetGfxPtr(bg3), LZ77Vram);
+	
+	dmaCopy(garlic_fontPal, BG_PALETTE, sizeof(garlic_fontPal));
+	
+	//generar los marcos de las ventanas de texto en el fondo 3
+	for (int i=0;i<NVENT;i++){
+		_gg_generarMarco(i);
+	}
+	//escalar los fondos 2 y 3 para que se ajusten exactamente a las dimensiones de una pantalla de la nds
+	//zoom al 50%
+	bgSetScale(bg3,520, 520);
+	bgSetScale(bg2, 520, 520);
+	bgUpdate();
 }
 
 
